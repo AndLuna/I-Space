ff2609a60b9caa4a6def92d799f3eeb362503213
AndLuna <contato.andreluna@gmail.com>
2023-02-09 14:24:26 -0300

    header home 1.0

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -107,3 +107,12 @@ window.addEventListener('click', function (event) {
    }
  });
 
+
+ //////// header
+
+ document.addEventListener('click',function(e){
+  // Hamburger menu
+  if(e.target.classList.contains('hamburger-toggle')){
+    e.target.children[0].classList.toggle('active');
+  }
+})   
