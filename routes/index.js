e855eec9ea12acac3700b5b267d3eacb0f572ea6
AndLuna <contato.andreluna@gmail.com>
2023-03-15 16:20:00 -0300

    rotas home, about e login ok

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,19 @@
 var express = require('express');
+
+
+//criando a const do controller
+const homeController = require('../controllers/homeController');
+
 var router = express.Router();
 
+
+
 /* GET home page. */
+
+
+
+router.get('/', homeController.home)
+
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
