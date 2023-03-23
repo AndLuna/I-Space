ac250fccf8dfd0056e02c262d9a0c3447a6c7346
AndLuna <contato.andreluna@gmail.com>
2023-03-23 11:21:39 -0300

    Somente uma pagina de produtos v1

--- src/controllers/PagesController.js
+++ src/controllers/PagesController.js
@@ -1,41 +1,15 @@
 const DB = require('../database/DB.json');
-
 const toThousand = n => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
 
-//const {createMenuObject} = require('../helper/createMenuObject');
 const PagesController = {
+    productPage: (req, res) => {
+        const { type } = req.params;
+        const filteredProducts = DB.filter((product) => product.type === type);
+        res.render('productPage', { products: filteredProducts, toThousand });
+      },
     sobre: (req, res) => {
         res.render('sobre', {});
-    },
-    
-
-    iphone: (req, res) => {
-        res.render('productsPage/iphone', {
-            DB,
-            toThousand
-        });
-    },
-
-    mac: (req, res) => {
-        res.render('productsPage/mac', {
-            DB,
-            toThousand
-        });
-    },
-
-    ipad: (req, res) => {
-        res.render('productsPage/ipad', {
-            DB,
-            toThousand
-        });
-    },
-
-    watch: (req, res) => {
-        res.render('productsPage/watch', {
-            DB,
-            toThousand
-        });
-    },
+    }
 };
 
 
--- src/routes/index.js
+++ src/routes/index.js
@@ -12,13 +12,15 @@ router.get('/', MainController.index);
 
 
 // # rota dos produtos
-router.get('/iphone', PagesController.iphone);
-router.get('/mac', PagesController.mac);
-router.get('/ipad', PagesController.ipad);
-router.get('/watch', PagesController.watch);
+// router.get('/iphone', PagesController.iphone);
+// router.get('/mac', PagesController.mac);
+// router.get('/ipad', PagesController.ipad);
+// router.get('/watch', PagesController.watch);
 
 // GET - EJS Detail producto - View
 router.get('/product/detail/:id', productController.detailEJS)
+router.get('/productPage/:type', PagesController.productPage);
+
 
 // # rota de paginas
 router.get('/sobre', PagesController.sobre);
