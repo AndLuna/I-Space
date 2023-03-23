7f31610955609e6ed763a7c5d013e81680ecd5c9
AndLuna <contato.andreluna@gmail.com>
2023-03-23 11:49:57 -0300

    Somente uma pagina de produtos v2

--- src/controllers/PagesController.js
+++ src/controllers/PagesController.js
@@ -1,11 +1,12 @@
-const DB = require('../database/DB.json');
+const DB = require('../database/products.json');
 const toThousand = n => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
 
 const PagesController = {
     productPage: (req, res) => {
         const { type } = req.params;
         const filteredProducts = DB.filter((product) => product.type === type);
-        res.render('productPage', { products: filteredProducts, toThousand });
+        const pageTitle = `Apple ${type}`;
+        res.render('productPage', { products: filteredProducts,  pageTitle: pageTitle, type: type, toThousand });
       },
     sobre: (req, res) => {
         res.render('sobre', {});
--- src/routes/index.js
+++ src/routes/index.js
@@ -11,12 +11,6 @@ const productController = require('../controllers/ProductController')
 router.get('/', MainController.index);
 
 
-// # rota dos produtos
-// router.get('/iphone', PagesController.iphone);
-// router.get('/mac', PagesController.mac);
-// router.get('/ipad', PagesController.ipad);
-// router.get('/watch', PagesController.watch);
-
 // GET - EJS Detail producto - View
 router.get('/product/detail/:id', productController.detailEJS)
 router.get('/productPage/:type', PagesController.productPage);
