d130bf101c376958f7460668cdc136b91c3f42f0
André Feijó <107944135+Alsf30@users.noreply.github.com>
2023-04-07 21:52:33 -0300

    Middlewares concluindo multer

--- src/middlewares/upload.js
+++ src/middlewares/upload.js
@@ -1,1 +1,12 @@
 const multer = require('multer');
+
+const storage = multer.diskStorage({
+    destination: function(req, file, cb) {
+      cb(null, 'public/images/produtos')
+    },
+    filename: function(req, file, cb) {
+      cb(null, Date.now() + "-" + file.originalname)
+    }
+  })
+  const upload = multer({ storage: storage })
+  
