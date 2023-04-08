d4d53fe9793594d61c909b9857f5e09adf074092
André Feijó <107944135+Alsf30@users.noreply.github.com>
2023-04-08 10:20:50 -0300

    Criando Middleware Log acessando rota do usuario

--- src/middlewares/log.js
+++ src/middlewares/log.js
@@ -2,6 +2,12 @@ const fs = require("fs");
 
 
 const log = (req,res,next) => {
-    const file = 'src\\logs\\logs.'
+    const file = 'src\\log\\logs.txt'
+
+    if(!fs.existsSync(file))
+        fs.writeFileSync(file,'')
+
+    fs.appendFileSync(file, 'O usuário acessou a rota: ' + req.url + '\n')
+    next()
 }
 module.exports = log
