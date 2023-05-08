ab8fbaa1f0944a3a554394c9f4d459f3799f546c
unknown <gabrielfdias082@gmail.com>
2023-05-08 18:47:33 -0300

    integração products model

--- src/controllers/MainController.js
+++ src/controllers/MainController.js
@@ -1,13 +1,23 @@
-const products = require('../database/products.json');
+/* const products = require('../database/products.json'); */
 const toThousand = n => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".")
+
+const {Product} = require('../models')
 
 
 const MainController = {
-    index: (req, res) => {
-        res.render('index', {
-            products,
-            toThousand
-        });
+    index: async(req, res) => {
+
+        try {
+            const products = await Product.findAll()
+            
+            res.render('index', {
+                products,
+                toThousand
+            });
+        } catch (error) {
+            res.status(400).json({error})
+        }
+
     }
 }
 
