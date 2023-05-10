fd527a226adf6d24ec04f9c0bc72c5cf6c07d302
unknown <gabrielfdias082@gmail.com>
2023-05-09 23:08:52 -0300

    product/user controller integrando com Database

--- src/models/Product.js
+++ src/models/Product.js
@@ -1,20 +1,30 @@
 module.exports = (sequelize, DataType) => {
-    const Product = sequelize.define('Product', {
-      id: {
-        type: DataType.INTEGER,
-        primaryKey: true,
-        autoIncrement: true
-      },
-      name: DataType.STRING(100),
-      price: DataType.DECIMAL(10, 2),
-      discount: DataType.INTEGER,
-      description: DataType.STRING(1000),
-      image: DataType.STRING(100),
-      id_product_type: DataType.INTEGER
-    }, {
-      timestamps: false,
-      tableName: 'product'
+  const Product = sequelize.define('Product', {
+    id: {
+      type: DataType.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    name: DataType.STRING(100),
+    price: DataType.DECIMAL(10, 2),
+    discount: DataType.INTEGER,
+    description: DataType.STRING(1000),
+    image: DataType.STRING(100),
+    id_product_type: DataType.INTEGER
+  }, {
+    tableName: 'product',
+    timestamps: false
+  })
+  Product.associate = (modelsList) => {
+    Product.belongsTo(modelsList.ProductType, {
+      foreignKey: 'id_product_type',
+      as: 'productType'
     })
-
-    return Product
+    Product.belongsToMany(modelsList.Order, {
+      foreignKey: 'id_order',
+      as: 'order',
+      through: modelsList.OrderItem
+    })
   }
+  return Product
+}
